5961c23fffc4d9dce435bdec99316bc920062f56
Carsen Decker <cad631@nyu.edu>
2018-10-22 02:36:18 -0400

    scoring system

--- Grid Game Clone/Assets/Scripts/RoundManager.cs
+++ Grid Game Clone/Assets/Scripts/RoundManager.cs
@@ -2,44 +2,60 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RoundManager : MonoBehaviour
 {
 
-	public int PointMax;
-	public Transform P1Spawn, P2Spawn;
-	public GameObject Player1, Player2;
-	public Text EndText, P1PointText, P2PointText;
+	public int MaxScore;
+	public Transform[] PlayerSpawnPoints;
+	public GameObject[] PlayerObjects;
+	public Text EndText;
+	public Text[] ScoreText;
 
-	private PlayerMovement p1Script, p2Script;
+	private PlayerMovement[] playerScripts = new PlayerMovement[2];
 	private bool betweenRounds = false;
-	private int p1Points, p2Points;
+	private int[] Scores = new int[2];
 	
 	// Use this for initialization
 	void Start ()
 	{
-		p1Script = Player1.GetComponent<PlayerMovement>();
-		p2Script = Player2.GetComponent<PlayerMovement>();
+		playerScripts[0] = PlayerObjects[0].GetComponent<PlayerMovement>();
+		playerScripts[1] = PlayerObjects[1].GetComponent<PlayerMovement>();
 
-		Player1.transform.position = P1Spawn.position;
-		Player2.transform.position = P2Spawn.position;
+		PlayerObjects[0].transform.position = PlayerSpawnPoints[0].position;
+		PlayerObjects[1].transform.position = PlayerSpawnPoints[1].position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			ResetGame();
+		}
+
+		ScoreText[0].text = Scores[0].ToString();
+		ScoreText[1].text = Scores[1].ToString();
+		
 		if (!betweenRounds)
 		{
-			if (p1Script.hitSomething)
+			if (playerScripts[0].hitSomething)
 			{
-//				P1PointText.gameObject.transform.position = Player1.transform.position;
-//				P1PointText.enabled = true;
-//				P2PointText.enabled = true;
+				Scores[0]++;
+				ScoreText[0].enabled = true;
+				ScoreText[1].enabled = true;
+			
 				StartCoroutine(NewRound());
 			}
-			else if (p2Script.hitSomething)
+			else if (playerScripts[1].hitSomething)
 			{
-//				EndText.enabled = true;
+				Scores[1]++;
+				ScoreText[0].enabled = true;
+				ScoreText[1].enabled = true;
+				
 				StartCoroutine(NewRound());
 			}
 		}
@@ -48,8 +64,8 @@ public class RoundManager : MonoBehaviour
 	IEnumerator NewRound()
 	{
 		betweenRounds = true;
-		p1Script.enabled = false;
-		p2Script.enabled = false;
+		playerScripts[0].enabled = false;
+		playerScripts[1].enabled = false;
 		
 		yield return new WaitForSeconds(2f);
 
@@ -58,17 +74,37 @@ public class RoundManager : MonoBehaviour
 		{
 			Destroy(wall);
 		}
-
-		Player1.transform.position = P1Spawn.position;
-		Player2.transform.position = P2Spawn.position;
 		
-		yield return new WaitForSeconds(1f);
-		
-		p1Script.enabled = true;
-		p2Script.enabled = true;
 
-		p1Script.hitSomething = false;
-		p2Script.hitSomething = false;
-		betweenRounds = false;
+		PlayerObjects[0].transform.position = PlayerSpawnPoints[0].position;
+		PlayerObjects[1].transform.position = PlayerSpawnPoints[1].position;
+
+		if (Scores[0] >= MaxScore || Scores[1] >= MaxScore)
+		{
+			EndText.enabled = true;
+			
+			ScoreText[0].gameObject.transform.position = EndText.transform.position + new Vector3(-60f, 0f, 0f);
+			ScoreText[1].gameObject.transform.position = EndText.transform.position + new Vector3(60f, 0f, 0f);
+		}
+		else
+		{
+			ScoreText[0].enabled = false;
+			ScoreText[1].enabled = false;
+			
+			yield return new WaitForSeconds(1f);
+
+			playerScripts[0].enabled = true;
+			playerScripts[1].enabled = true;
+
+
+			playerScripts[0].hitSomething = false;
+			playerScripts[1].hitSomething = false;
+			betweenRounds = false;
+		}
+	}
+
+	void ResetGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 }
